272ec0bdc91c190ae7b763a10d209a90bf5e35e9
e.willhardy <e.willhardy@a38268c0-e270-11de-b41e-e93f42a66952>
2010-06-03 20:37:56 +0000

    Fixed issue 3: Changing encoding declaration.
    git-svn-id: https://django-seo-tools.googlecode.com/svn/trunk@28 a38268c0-e270-11de-b41e-e93f42a66952

--- seo/admin.py
+++ seo/admin.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 from django.contrib import admin
 from django.contrib.contenttypes import generic
--- seo/middleware.py
+++ seo/middleware.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 # Copyright 2009 justquick202
 # Licenced under MIT Licence
