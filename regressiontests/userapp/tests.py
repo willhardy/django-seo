d1b9a1c1ba233ae3b285da20daddae4df3d9b7d1
Will Hardy <will@willhardy.com.au>
2011-01-23 20:21:41 +0100

    Fixing test for non-pathed models.

--- regressiontests/userapp/tests.py
+++ regressiontests/userapp/tests.py
@@ -737,20 +737,22 @@ class Templates(TestCase):
         self.compilesTo("{% get_metadata for obj as var %}{{ var }}", unicode(self.metadata))
 
     def test_for_obj_no_path(self):
+        self.deregister_alternatives()
+
         # NoPath objects can exist without a matching metadata instance
         obj1 = NoPath.objects.create()
         obj2 = NoPath.objects.create()
         content_type = ContentType.objects.get_for_model(NoPath)
         obj_metadata = Coverage._meta.get_model('modelinstance').objects.create(_content_type=content_type, _object_id=obj2.id, title="Correct Title")
 
+        self.context = {'obj': obj2}
+        self.compilesTo("{% get_metadata for obj %}", unicode(obj_metadata))
+        self.compilesTo("{% get_metadata for obj as var %}{{ var }}", unicode(obj_metadata))
+
         # Where the object does not link to a metadata object, defaults should be returned
         self.context = {'obj': obj1}
         self.compilesTo("{% get_metadata for obj %}", "<title>example.com</title>")
         self.compilesTo("{% get_metadata for obj as var %}{{ var }}", "<title>example.com</title>")
-
-        self.context = {'obj': obj2}
-        self.compilesTo("{% get_metadata for obj %}", unicode(obj_metadata))
-        self.compilesTo("{% get_metadata for obj as var %}{{ var }}", unicode(obj_metadata))
 
     def test_wrong_class_name(self):
         self.compilesTo("{% get_metadata WithSites %}", "")
