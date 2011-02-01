8c8dc9ff518007c98e5f612dcbc9ceea50171763
Will Hardy <will@willhardy.com.au>
2011-02-01 12:39:01 +0100

    Cleaning up templatetag, using apparently more standard approach for getting path.

--- rollyourown/seo/templatetags/seo.py
+++ rollyourown/seo/templatetags/seo.py
@@ -11,13 +11,19 @@ class MetadataNode(template.Node):
     def __init__(self, metadata_name, variable_name, target, site, language):
         self.metadata_name = metadata_name
         self.variable_name = variable_name
-        self.target = template.Variable(target or 'request.META.PATH_INFO')
+        self.target = template.Variable(target or 'request.path')
         self.site = site and template.Variable(site) or None
         self.language = language and template.Variable(language) or None
 
     def render(self, context):
         try:
             target = self.target.resolve(context)
+        except VariableDoesNotExist:
+            msg = (u"{% get_metadata %} needs some path information.\n"
+                        u"Please use RequestContext with the django.core.context_processors.request context processor.\n"
+                        "Or provide a path or object explicitly, eg {% get_metadata for path %} or {% get_metadata for object %}")
+            raise template.TemplateSyntaxError(msg)
+        else:
             if callable(target):
                 target = target()
             if isinstance(target, basestring):
@@ -28,11 +34,6 @@ class MetadataNode(template.Node):
                 path = target['get_absolute_url']()
             else:
                 path = None
-        except VariableDoesNotExist:
-            msg = (u"{% get_metadata %} needs some path information.\n"
-                        u"Please use RequestContext with the django.core.context_processors.request context processor.\n"
-                        "Or provide a path or object explicitly, eg {% get_metadata for path %} or {% get_metadata for object %}")
-            raise template.TemplateSyntaxError(msg)
 
         kwargs = {}
 
@@ -70,7 +71,7 @@ def do_get_metadata(parser, token):
 
         or if you have multiple metadata classes:
 
-        {% get_metadata MyClass [as my_variable] %}
+        {% get_metadata MyClass [for my_path] [in my_language] [on my_site] [as my_variable] %}
 
     """
     bits = list(token.split_contents())
@@ -79,16 +80,25 @@ def do_get_metadata(parser, token):
     metadata_name = None
     args = { 'as': None, 'for': None, 'in': None, 'on': None }
 
+    # If there are an even number of bits, 
+    # a metadata name has been provided.
     if len(bits) % 2:
         metadata_name = bits[0]
         bits = bits[1:]
+
+    # Each bits are in the form "key value key value ..."
+    # Valid keys are given in the 'args' dict above.
     while len(bits):
         if len(bits) < 2 or bits[0] not in args:
             raise template.TemplateSyntaxError("expected format is '%r [as <variable_name>]'" % tag_name)
-        args[bits[0]] = bits[1]
-        bits = bits[2:]
+        key, value, bits = bits[0], bits[1], bits[2:]
+        args[key] = value
 
-    return MetadataNode(metadata_name, args['as'], args['for'], args['on'], args['in'])
+    return MetadataNode(metadata_name, 
+                variable_name = args['as'], 
+                target = args['for'], 
+                site = args['on'], 
+                language = args['in'])
 
 
 register.tag('get_metadata', do_get_metadata)
